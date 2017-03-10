e7a87602cafacb3b3789669679b68924f7ea9706
Giedrius <lauruska.giedrius@gmail.com>
2017-03-10 17:32:01 +0200

    Add navigation

---
+++ app/components/Countdown.jsx
@@ -0,0 +1,14 @@
+var React = require('react');
+
+var Countdown = React.createClass({
+  render: function () {
+    return(
+      <div>
+        <h3>Countdown.jsx</h3>
+      </div>
+    )
+  }
+});
+
+
+module.exports = Countdown;
